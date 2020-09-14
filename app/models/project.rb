765d8f42e1295cd0674b0dc2dd9b90781a5e877b
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-09-14 18:08:40 +0200

    seting a main menu at top

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@ class Project < ApplicationRecord
     validates :title, presence: true 
     validates :description, presence: true, length: { :minimum => 5 }
 
+    belongs_to :user
+
     def to_s 
         title 
     end
